027523b2027474557c590a57b85d5ec23a4fee11
Peperoni <wild2me35@gmail.com>
2023-08-29 10:52:14 -0400

    Fixing splitting ':' in event, periods with minus and event with links

--- main.py
+++ main.py
@@ -22,14 +22,23 @@ def get_data():
     html_with_dates = target_tag_parent.find_next_siblings()
     for date in html_with_dates:
         date_clean = str(date).replace('<p>', '').replace('</p>', '').replace('–', '-')
+        if '<a' in date_clean and 'résumé' not in date_clean and date_clean[0] != '→':
+            print(date_clean)
+            date_clean_split = date_clean.split('<')
+            date_clean = date_clean_split[0]
+            date_clean_split_event = date_clean_split[1].split('>')
+            date_clean += date_clean_split_event[1]
         if '<' not in date_clean and 'Voir aussi' not in date_clean:
             date_split = date_clean.split(' : ')
+            event = date_split[1]
+            if len(date_split) > 2:
+                event += ' ' + date_split[2]
             year_clean = date_split[0].replace(' ', '')
-            event_clean_lower_case = date_split[1].replace('.', '')
+            event_clean_lower_case = event.replace('.', '')
             event_clean = event_clean_lower_case[0].upper() + event_clean_lower_case[1:]
             if '-' in year_clean and year_clean[0] != '-':
                 split_period('-', year_clean, event_clean, dates_json)
-            elif (year_clean[0] != '-') and '-' in year_clean[1:]:
+            elif year_clean[0] == '-' and '-' in year_clean[1:]:
                 split_period_avjc(year_clean, event_clean, dates_json)
             else:
                 if 'v.' in year_clean:
@@ -48,11 +57,11 @@ def split_period(character, date, event, data):
     years = date.split(character)
     new_date_begin = {
         "year": years[0],
-        "event": 'Début ' + event
+        "event": 'Début ' + event[0].lower() + event[1:]
     }
     new_date_end = {
         "year": years[1],
-        "event": 'Fin ' + event
+        "event": 'Fin ' + event[0].lower() + event[1:]
     }
     data["dates"].append(new_date_begin)
     data["dates"].append(new_date_end)
@@ -62,11 +71,11 @@ def split_period_avjc(date, event, data):
     years = date[1:].split('-')
     new_date_begin = {
         "year": '-' + years[0],
-        "event": 'Début ' + event
+        "event": 'Début ' + event[0].lower() + event[1:]
     }
     new_date_end = {
         "year": '-' + years[1],
-        "event": 'Fin ' + event
+        "event": 'Fin ' + event[0].lower() + event[1:]
     }
     data["dates"].append(new_date_begin)
     data["dates"].append(new_date_end)
@@ -75,9 +84,9 @@ def split_period_avjc(date, event, data):
 # TODO Manage months -> DONE on frontend
 # TODO Replace american dashes by french dashes and spaces by void string -> DONE
 # TODO Retrieve points and add upper case on first letter -> DONE
+# TODO 907 : Chine : le dernier empereur Tang est détrôné. deal with the punctuaction in the middle -> DONE
+# TODO Bug dates with 2 numbers av JC -> DONE
 
-# TODO 907 : Chine : le dernier empereur Tang est détrôné. deal with the punctuaction in the middle
-# TODO Bug dates with 2 numbers av JC
 # TODO Do not delete dates with links
 
 get_data()
