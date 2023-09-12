8f07dbd934eaa7c8c3e31368537bc7a7549254f1
Peperoni <wild2me35@gmail.com>
2023-09-12 17:30:00 -0400

    Modifying the homepage to put the logo and the name

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,16 @@
 </script>
 
 <template>
-  <div class="flex justify-center items-center h-screen text-center">
-    <router-link to="/quiz"><button class="btn">Commencer le quiz</button></router-link>
+  <div class="justify-center flex flex-col items-center h-screen text-center">
+    <div class="justify-center flex items-center mb-5">
+      <img class="w-20" src="../assets/book_red.svg" />
+      <h1 class="text-5xl mb-2 text-black-500 font-bold">
+        Quiz d'Hérodote
+      </h1>
+    </div>
+    <div>
+      <router-link to="/quiz"><button class="btn">Commencer le quiz</button></router-link>
+    </div>
   </div>
 </template>
 
